f11605b26e8b8b2632859170d54955f2412c6a47
unsafer <luismd.arica@gmail.com>
2014-12-11 00:02:01 -0300

    Creacion proyecto 2
    se crea proyecto

---
+++ src/termodinamica/Termodinamica.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package termodinamica;
+
+/**
+ *
+ * @author Unsafer
+ */
+public class Termodinamica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
